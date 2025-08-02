bfc37ad90f514eed8e8c92b217b517fa7c6d28ea
Ariq Chowdhury <101491244+AriqGChowdhury@users.noreply.github.com>
2025-08-02 14:37:00 -0400

    Update settings.py

--- ai_interviewer/settings.py
+++ ai_interviewer/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%-5sn2l(rpa9bb_ip=9a9$j3qyrc3hrl9#zme9ys_fag42=$i-'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["ai-interviewer-7eqr.onrender.com"]
 
